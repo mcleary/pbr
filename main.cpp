3b553ebdcd757dc1e714444586254450442c663d
Thales Sabino <tluisrs@gmail.com>
2016-03-27 20:55:19 -0300

    Attempt to write a FPSCounter class
    std::chrono library is a bit complicated.
    Need to study a little more

--- main.cpp
+++ main.cpp
@@ -7,9 +7,53 @@ using namespace gl;
 
 #include <GLFW/glfw3.h>
 
+#include <chrono>
+
+class FPSCounter
+{
+public:
+    FPSCounter() :
+        m_RefreshRate(500),
+        m_CurrentTime(0),
+        m_FrameTime(0)
+    {
+    }
+    
+    void startFrame()
+    {
+        auto now = std::chrono::system_clock::now();
+        auto nowMilliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(now).count();
+        
+    }
+    
+    bool update()
+    {
+        m_FrameCount++;
+        
+        if(m_CurrentTime > m_RefreshRate)
+        {
+            auto currentFPS = m_FrameCount / std::chrono::duration_cast<std::chrono::milliseconds>(m_CurrentTime).count();
+            
+            std::cout << currentFPS << std::endl;
+            
+            m_CurrentTime = std::chrono::milliseconds(0);
+            m_FrameCount = 0;
+            
+            return true;
+        }
+        
+        return false;
+    }
+    
+private:
+    std::chrono::milliseconds m_RefreshRate;
+    std::chrono::milliseconds m_CurrentTime;
+    std::chrono::milliseconds m_FrameTime;
+    int                       m_FrameCount    = 0;
+};
+
 void draw()
 {
-    glClearColor(1.0, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
@@ -33,6 +77,8 @@ static void init()
     cout << "\tGL_VERSION:  " << glGetString(GL_VERSION) << endl;
     cout << "\tGL_VENDO: " << glGetString(GL_VENDOR) << endl;
     //cout << "\tGL_EXTENSIONS: " << glGetString(GL_EXTENSIONS) << endl;
+    
+    glClearColor(1.0, 0.0, 0.0, 1.0);
 }
 
 int main()
@@ -40,20 +86,20 @@ int main()
     GLFWwindow* window;
     int width, height;
     
-    if( !glfwInit() )
+    if(!glfwInit())
     {
         std::cerr << "Failed to initialize GLFW" << std::endl;
         fprintf( stderr, "Failed to initialize GLFW\n" );
         
     }
     
-    glbinding::Binding::initialize();
-    
     glfwWindowHint(GLFW_DEPTH_BITS, 24);
     glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 2);
-//    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-//    glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, 1);
+    glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    glbinding::Binding::initialize();
     
     window = glfwCreateWindow( 800, 600, "Physically Based Rendering with OpenGL", NULL, NULL );
     if (!window)
@@ -77,6 +123,8 @@ int main()
     // Parse command-line options
     init();
     
+    FPSCounter fpsCounter;
+    
     // Main loop
     while( !glfwWindowShouldClose(window) )
     {
@@ -89,6 +137,8 @@ int main()
         // Swap buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
+        
+        fpsCounter.update();
     }
     
     // Terminate GLFW
